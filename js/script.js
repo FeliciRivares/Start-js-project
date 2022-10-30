d9ec6a1eed3bfdb274bf20a17e25fe3c412230a5
Karina <karina.ostrovsika@gmail.com>
2022-10-30 21:23:40 +0200

    loops and ternary operators were added to check the conditions of writing user responses

--- js/script.js
+++ js/script.js
@@ -1,13 +1,6 @@
 "use strict";
 
-
-
-const namberOfFilms = +prompt ("how mane films do you see?", ""),
-
-    moviesOneLastMovie = prompt ("what's the last movie you watched?", ""),
-    moviesOneRate = +prompt ("How much would you rate this movie?", ""),
-    moviesTwoLastMovie = prompt ("what's the last movie you watched?", ""),
-    moviesTwoRate = +prompt ("How much would you rate this movie?", "");
+const namberOfFilms = +prompt ("how mane films do you see?", "");
 
 const personalMovieDB = {
     count: namberOfFilms,
@@ -15,9 +8,33 @@ const personalMovieDB = {
     actors: {},
     genres: [],
     privat: false
-
 };
-personalMovieDB.movies[moviesOneLastMovie] = moviesOneRate;
-personalMovieDB.movies[moviesTwoLastMovie] = moviesTwoRate;
+
+for(let i = 0; i < 2; i++) {
+  const  moviesOneLastMovie = prompt ("what's the last movie you watched?", "");
+  const  moviesOneRate = +prompt ("How much would you rate this movie?", "");
+
+    if (moviesOneLastMovie != null && moviesOneRate != null && moviesOneLastMovie !="" && 
+        moviesOneRate !="" && moviesOneLastMovie.length < 50)
+    {
+        personalMovieDB.movies[moviesOneLastMovie] = moviesOneRate;
+        console.log("done");
+    } else {
+        console.log("erro");
+        i--;
+    }
+
+}
+
+if(personalMovieDB.count < 10){
+    console.log("watched few movies");
+    }else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30){
+        console.log("you are a wonderful viewer");
+    }else if (personalMovieDB.count >= 30){
+        console.log("you are a movie buff");
+    }else{
+        console.log("Error");
+    }
+
 
 console.log(personalMovieDB);
