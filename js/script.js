c84129c35ffa91c96c304f0388a007fca17c7b4d
Karina <karina.ostrovsika@gmail.com>
2022-10-31 16:03:27 +0200

    add functions and additions array of favorite genres

--- js/script.js
+++ js/script.js
@@ -1,6 +1,30 @@
 "use strict";
 
-const namberOfFilms = +prompt ("how mane films do you see?", "");
+let namberOfFilms;
+function start(){
+    namberOfFilms = prompt ("how mane films do you see?", "");
+    while(namberOfFilms == '' || namberOfFilms == null || isNaN(namberOfFilms)){
+        namberOfFilms = prompt ("how mane films do you see?", "");
+    }
+}
+start();
+
+function showMyDB(hidden){
+    if(!hidden){
+        console.log(personalMovieDB);
+
+    }
+}
+
+
+function writeYourGenres(){
+    for(let i = 1; i <= 3; i++) {
+        personalMovieDB.genres[i - 1] = prompt (`your favorite genre under the number ${i} `);
+      }
+    
+}
+
+
 
 const personalMovieDB = {
     count: namberOfFilms,
@@ -9,32 +33,37 @@ const personalMovieDB = {
     genres: [],
     privat: false
 };
+writeYourGenres();
+function rememberMyFilms(){
+    for(let i = 0; i < 2; i++) {
+        const  moviesOneLastMovie = prompt ("what's the last movie you watched?", "");
+        const  moviesOneRate = +prompt ("How much would you rate this movie?", "");
+      
+          if (moviesOneLastMovie != null && moviesOneRate != null && moviesOneLastMovie !="" && 
+              moviesOneRate !="" && moviesOneLastMovie.length < 50)
+          {
+              personalMovieDB.movies[moviesOneLastMovie] = moviesOneRate;
+              console.log("done");
+          } else {
+              console.log("erro");
+              i--;
+          }
+      
+      }
+}
+rememberMyFilms();
 
-for(let i = 0; i < 2; i++) {
-  const  moviesOneLastMovie = prompt ("what's the last movie you watched?", "");
-  const  moviesOneRate = +prompt ("How much would you rate this movie?", "");
-
-    if (moviesOneLastMovie != null && moviesOneRate != null && moviesOneLastMovie !="" && 
-        moviesOneRate !="" && moviesOneLastMovie.length < 50)
-    {
-        personalMovieDB.movies[moviesOneLastMovie] = moviesOneRate;
-        console.log("done");
-    } else {
-        console.log("erro");
-        i--;
-    }
-
+function detectPersonalLevel(){
+    if(personalMovieDB.count < 10){
+        console.log("watched few movies");
+        }else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30){
+            console.log("you are a wonderful viewer");
+        }else if (personalMovieDB.count >= 30){
+            console.log("you are a movie buff");
+        }else{
+            console.log("Error");
+        }
 }
 
-if(personalMovieDB.count < 10){
-    console.log("watched few movies");
-    }else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30){
-        console.log("you are a wonderful viewer");
-    }else if (personalMovieDB.count >= 30){
-        console.log("you are a movie buff");
-    }else{
-        console.log("Error");
-    }
-
-
-console.log(personalMovieDB);
+detectPersonalLevel();
+showMyDB(personalMovieDB.privat);
